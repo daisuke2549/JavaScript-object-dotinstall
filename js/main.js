2c8e6fea334917445285b9c6cc42c43dbdad2ccb
daisuke sasaki <gerund25495641@gmail.com>
2021-02-21 08:55:12 +0900

    配列push,shiftまで実装済み

--- js/main.js
+++ js/main.js
@@ -1,15 +1,57 @@
-"use strict";
+/*配列*/
+
+
+'use strict';
+
+// {
+//   // const score1 = 80;
+//   // const score2 = 90;
+//   // const score3 = 40;
+
+//   const scores = [80, 90, 40];
+// //   console.log(scores[0]);
+
+
+// //   scores[2] = 100;
+//   console.log(scores);
+
+//   console.log(scores.length);
+
+// }
 
 
 
-function add(x, ...y){
-    let result = x;
+{
+  const scores = [80, 90, 40, 70];
+  scores.push(10,30);
+  scores.shift();
 
-    y.forEach(number => {
-        result = result + number;
-    });
-    console.log(result);
+  
+
+
+
+  for (let i = 0; i < scores.length; i++) {
+    console.log(`Score ${i}: ${scores[i]}`);
+  }
+
+
+
 }
 
 
-add(1,2,3,4);
+
+
+
+
+
+/*配列*/
+
+
+
+
+
+/*配列*/
+
+
+
+
