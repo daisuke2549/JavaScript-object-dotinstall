a4f043cedeb31318dfba82003e5d9398e403ffc5
daisuke sasaki <gerund25495641@gmail.com>
2021-02-21 09:31:59 +0900

    クラス定義済み

--- js/main.js
+++ js/main.js
@@ -73,12 +73,52 @@
 
 
 
-
-
-
-
 /*クラス*/
 
+'use strict';
+// {
+//     class Post {
+//       constructor(text) {
+//         this.text = text;
+//         this.likeCount = 0;
+//       }
+  
+//       show() {
+//         console.log(`${this.text} - ${this.likeCount}いいね`);
+//       }
+//     }
+  
+//     const posts = [
+//       new Post('JavaScriptの勉強中…'),
+//       new Post('プログラミング楽しい！'),
+//     ];
+  
+//     posts[0].show();
+//     posts[1].show();
+// }
+
+
+
+'use strict';
+
+class User{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    getName(){
+        return this.name;
+    }
+    getAge(){
+        return this.age;
+    }
+}
+
+
+
+let taro = new User("鈴木", 32);
+console.log(taro.getName());
+console.log(taro.getAge());
 
 
 
