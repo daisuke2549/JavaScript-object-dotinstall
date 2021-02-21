815e8d67a1745d1c487cf7b7d1aa81e95de744c1
daisuke sasaki <gerund25495641@gmail.com>
2021-02-21 09:11:31 +0900

    foreachまで実装済み

--- js/main.js
+++ js/main.js
@@ -21,21 +21,54 @@
 
 
 
-{
-  const scores = [80, 90, 40, 70];
-  scores.push(10,30);
-  scores.shift();
-
+// {
+//   const scores = [80, 90, 40, 70];
+// //   scores.push(10,30);  /*後ろに要素を追加*/
+// //   scores.shift();      /*1番前の要素を削除 */
+  
+//   scores.splice(1, 1, 40, 50, 100);
   
 
 
 
-  for (let i = 0; i < scores.length; i++) {
-    console.log(`Score ${i}: ${scores[i]}`);
-  }
+//   for (let i = 0; i < scores.length; i++) {
+//     console.log(`Score ${i}: ${scores[i]}`);
+//   }
 
 
 
+// }
+
+
+
+
+// 'use strict';
+
+// {
+//     const otherScores = [10, 20];
+//     const scores = [80, 90, 10, 30, ...otherScores];
+//     console.log(scores);
+
+
+//    function sum(a,b){
+//        console.log(a+b);
+//    }
+
+//   sum(...otherScores);
+
+
+// }
+
+/*foreach*/
+'use strict';
+
+{
+    const scores = [10, 20, 40, 90];
+
+
+    scores.forEach((score, index) => {
+        console.log(`Score ${index}: ${score}`);
+      });
 }
 
 
@@ -44,9 +77,18 @@
 
 
 
+/*クラス*/
+
+
+
+
+
+
+
+
+
 /*配列*/
-
-
+/*配列*/
 
 
 
